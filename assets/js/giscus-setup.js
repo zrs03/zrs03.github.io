26086f343206b878f1df9f04ddace552b88f253d
zrs03 <yyxlabc@gmail.com>
2026-05-20 18:31:19 +0000

    Deploying to gh-pages from @ zrs03/zrs03.github.io@a253e08c0f39be434294019f12f6e5b7f761e9be 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zrs03/Personal-Page",
+    "data-repo": "zrs03/zrs03.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
